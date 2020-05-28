9ec69b08b14da1b55a9ecde1f2f62fee2381e897
Da-Colon <dcolonantonio@gmail.com>
2020-05-28 08:01:25 -0400

    added actions to splash button.

--- src/game/Controller.java
+++ src/game/Controller.java
@@ -1,7 +1,26 @@
 package game;
 
-public class Controller {
-    public static void main(String[] args){
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+public class Controller {
+
+    @FXML
+    private void handleGameStart(MouseEvent event) throws Exception{
+        final Node source = (Node) event.getSource();
+        Parent gameScene = FXMLLoader.load(getClass().getResource("game.fxml"));
+        final Stage stage = (Stage) source.getScene().getWindow();
+        stage.setScene(new Scene(gameScene, 640, 480));
+        
+    }
+
+    @FXML
+    private void handleAppClose(MouseEvent event) {
+        System.exit(0);
     }
 }
--- src/game/Game.java
+++ src/game/Game.java
@@ -1,13 +1,16 @@
 package game;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.text.Text;
 
+import java.net.URL;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Random;
+import java.util.ResourceBundle;
 
-public class Game {
+public class Game implements Initializable {
     boolean running = false;
     Random random = new Random();
 
@@ -17,23 +20,26 @@ public class Game {
     @FXML
     private Text dealerHand;
 
-    public void GameEngine(){
+    public void initialize(URL location, ResourceBundle resources){
+        gameEngine();
+    }
+    
+    public void gameEngine(){
         running = true;
         Player player = new Player("Challenger");
         Dealer dealer = new Dealer();
         Deck deckObject = new Deck();
         Card[] deck = deckObject.newDeck();
-
+        
         for(int i = 0; i < 2; i++){
             player.hand[i] = deck[random.nextInt(51)];
             dealer.hand[i] = deck[random.nextInt(51)];
         }
-
-//        Game Loop Starts Here
+        
+        //        Game Loop Starts Here
         Card.displayCurrentHand(player.hand, player.handTotal, dealer.hand, dealer.handTotal);
-//        text.setText(Card.displayPlayerHand(player.hand));
-//        Card.displayDealerHand(dealer.hand, dealer.handTotal);
-
-
+        // text.setText(Card.displayPlayerHand(player.hand));
+        // Card.displayDealerHand(dealer.hand, dealer.handTotal);
+        
     }
 }
